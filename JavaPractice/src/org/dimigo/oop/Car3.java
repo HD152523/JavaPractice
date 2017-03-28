b9c4cb13ff77b97c4982c662e89b89542de1d4bc
HD152523 <junho7108@naver.com>
2017-03-28 12:55:29 +0900

    car3

---
+++ JavaPractice/src/org/dimigo/oop/Car3.java
@@ -0,0 +1,68 @@
+/**
+ * 
+ */
+package org.dimigo.oop;
+
+/**
+ * <pre>
+ * org.dimigo.oop
+ *  |_Car
+ * 
+ * 1. 개요 : 
+ * 2. 작성일 : 2017. 3. 23.
+ * </pre>
+ * @author : junho
+ * @version : 1.0
+ */
+public class Car3 {
+    private String company = "";
+    private String model = "";
+    private String color = "";
+    private int maxSpeed = 0;
+    private int price = 0;
+
+    /**
+	 * @param string
+	 * @param setCompany
+	 */
+	public Car3() {
+	}
+    public Car3 (String company,String model, String color, int maxSpeed, int price)
+    {
+        this.company = company;
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+        this.price = price;
+    }
+    public Car3 (String company,String model, String color, int maxSpeed)
+    {
+        this.company = company;
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+    public Car3 (String company,String model, String color)
+    {
+        this.company = company;
+        this.model = model;
+        this.color = color;
+    }
+
+	public String getCompany(){
+        return company;
+    }
+    public String getModel(){
+        return model;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getMaxSpeed(){
+        return maxSpeed;
+    }
+    public int getPrice() {return price;}
+
+
+
+}
