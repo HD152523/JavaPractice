b58d327408e775f8a3c2a5ec80069c7670df1d70
HD152523 <junho7108@naver.com>
2017-05-11 09:22:19 +0900

    Add files via upload

---
+++ JavaPractice/src/org/dimigo/inheritance/PersonTest2.java
@@ -0,0 +1,26 @@
+package org.dimigo.inheritance;
+
+public class PersonTest2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Person[] persons = {
+				new Person(),
+				new Korean("홍길동"),
+				new Japanese("다나카"),
+				new Chinese("왕밍")
+		};
+		for(Person p : persons){
+			greeting(p);
+		}
+		
+		
+	}
+	private static void greeting (Person p) {
+		System.out.println(p);
+		p.sayHello();
+		p.sayBye();
+		System.out.println("");
+	}
+
+}
